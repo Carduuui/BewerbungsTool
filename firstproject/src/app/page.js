38d030e918ec8ef1bb68a8dec13064388f52da05
Carduuui <eckstein.jakob1@gmail.com>
2025-06-29 17:04:57 +0200

    Gemini API

--- firstproject/src/app/page.js
+++ firstproject/src/app/page.js
@@ -1,8 +1,44 @@
+'use client'
+import {useState} from "react";
 import styles from "./globals.css";
 
 export default function Home() {
+  const prompt = "Write quote of the day.";
+
+  const [output, setOutput] = useState('This is a nextjs project');
+
+  const generateText = async () => {
+    console.log("clicked");
+    try{
+      const response = await fetch("/api/generate", {
+        method: 'POST',
+        headers:{
+          'Content-type' : 'application/json'
+        },
+        body: JSON.stringify({body:prompt})
+      });
+
+      const data = await response.json();
+
+      if(response.ok){
+        setOutput(data.output);
+      }
+      else{
+        setOutput(data.error);
+        const text = await response.text();
+        console.error("Server error:", response.status, text);
+      }
+    }
+    catch(err){
+      console.error(err)
+    }
+  }
+
   return (
-    <MyButton />
+    <div>    
+      <p onClick={generateText}>{output}</p>
+      <MyButton />
+    </div>
   );
 }
 
